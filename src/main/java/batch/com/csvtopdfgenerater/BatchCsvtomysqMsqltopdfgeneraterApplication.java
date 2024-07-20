506deb2bedf248d014a001ef540e42a2f34475e4
sirajprojects <87057875+sirajprojects@users.noreply.github.com>
2024-07-21 04:00:01 +0530

    add

---
+++ src/main/java/batch/com/csvtopdfgenerater/BatchCsvtomysqMsqltopdfgeneraterApplication.java
@@ -0,0 +1,13 @@
+package batch.com.csvtopdfgenerater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchCsvtomysqMsqltopdfgeneraterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchCsvtomysqMsqltopdfgeneraterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/batch/com/csvtopdfgenerater/BatchCsvtomysqMsqltopdfgeneraterApplicationTests.java
@@ -0,0 +1,13 @@
+package batch.com.csvtopdfgenerater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchCsvtomysqMsqltopdfgeneraterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
